b50e129a2feff717f7de42fd3e6472189ecb69f3
Bhavankumarg <gbhavanbkr@gmail.com>
2024-12-31 14:42:05 +0530

    2024 dynamic

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,14 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
-import { FaArrowRight, FaInstagram, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import {
+  FaArrowRight,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedinIn,
+} from "react-icons/fa";
 // import { AiOutlineDown } from "react-icons/ai";
 import { IoMdArrowDropdown } from "react-icons/io";
 
@@ -18,19 +23,27 @@ const Footer = () => {
                 About Us <IoMdArrowDropdown className="ml-2" />
               </h3>
               <div className="lg:w-[175px] w-[300px] h-[1px] bg-customGrayMd my-3"></div>
-              <Link href="/our-story/" className="flex gap-10 hover:text-red-600 group">
+              <Link
+                href="/our-story/"
+                className="flex gap-10 hover:text-red-600 group"
+              >
                 <li className="flex-grow">Our Story</li>
                 <FaArrowRight className="mt-1 me-20 " />
               </Link>
-              <Link href="/our-team" className="flex gap-9 mt-2 hover:text-red-600 group">
+              <Link
+                href="/our-team"
+                className="flex gap-9 mt-2 hover:text-red-600 group"
+              >
                 <li className="flex-grow">Our Team</li>
                 <FaArrowRight className="mt-1 me-20  " />
               </Link>
               <div className="lg:w-[175px] w-[300px] h-[1px] bg-customGrayMd my-3"></div>
               <li className="mt-4">
-              <Link href="/what-we-do">
-                <h3 className="text-white text-2xl hover:text-red-600">What We Do</h3>
-              </Link>
+                <Link href="/what-we-do">
+                  <h3 className="text-white text-2xl hover:text-red-600">
+                    What We Do
+                  </h3>
+                </Link>
               </li>
             </ul>
             {/* <ul className="text-customGrayMd text-xl mb-8 lg:mb-0">
@@ -41,26 +54,38 @@ const Footer = () => {
             </ul> */}
             <ul className="text-customGrayMd text-xl mb-8 lg:mb-0">
               <h3 className="text-white text-2xl flex items-center">
-                Insights <IoMdArrowDropdown className="ml-2 float-end -end-10" />
+                Insights{" "}
+                <IoMdArrowDropdown className="ml-2 float-end -end-10" />
               </h3>
               <div className="lg:w-[220px] w-[300px] h-[1px] bg-customGrayMd my-3"></div>
-              <Link href="/impact-stories" className="flex items-center justify-between text-white/60 mt-2 hover:text-red-600 group">
+              <Link
+                href="/impact-stories"
+                className="flex items-center justify-between text-white/60 mt-2 hover:text-red-600 group"
+              >
                 <li className="flex-grow">Impact Stories</li>
                 <FaArrowRight className="me-20  " />
               </Link>
-              <Link href="/blogs" className="flex items-center justify-between text-white/60 hover:text-red-600 group mt-2">
+              <Link
+                href="/blogs"
+                className="flex items-center justify-between text-white/60 hover:text-red-600 group mt-2"
+              >
                 <li className="flex-grow">Stories (Blogs)</li>
                 <FaArrowRight className="me-20  " />
               </Link>
-              
-              <Link href="/news-and-publications" className="flex items-center justify-between text-white/60 mt-2 hover:text-red-600 group">
+
+              <Link
+                href="/news-and-publications"
+                className="flex items-center justify-between text-white/60 mt-2 hover:text-red-600 group"
+              >
                 <li className="flex-grow me-3">News and Publications</li>
                 <FaArrowRight className="me-20  " />
               </Link>
             </ul>
             <ul className="text-white/60 text-xl">
               <Link href="/contact-us">
-                <h3 className="text-white text-2xl hover:text-red-600">Contact Us</h3>
+                <h3 className="text-white text-2xl hover:text-red-600">
+                  Contact Us
+                </h3>
               </Link>
               <div className="lg:w-[175px] w-[300px] h-[1px] bg-customGrayMd my-3"></div>
               <Link href="mailto:Info@upfrontglobal.com">
@@ -69,13 +94,25 @@ const Footer = () => {
                 </li>
               </Link>
               <li className="flex pt-5 gap-5 text-3xl ">
-                <Link target="_blank" href="https://www.linkedin.com/showcase/upfrontcg/" className="hover:text-white">
+                <Link
+                  target="_blank"
+                  href="https://www.linkedin.com/showcase/upfrontcg/"
+                  className="hover:text-white"
+                >
                   <FaLinkedinIn />
                 </Link>
-                <Link target="_blank" href="https://twitter.com/UpfrontCG" className="hover:text-white">
+                <Link
+                  target="_blank"
+                  href="https://twitter.com/UpfrontCG"
+                  className="hover:text-white"
+                >
                   <FaTwitter />
                 </Link>
-                <Link target="_blank" href="https://www.instagram.com/upfront.global/" className="hover:text-white">
+                <Link
+                  target="_blank"
+                  href="https://www.instagram.com/upfront.global/"
+                  className="hover:text-white"
+                >
                   <FaInstagram />
                 </Link>
               </li>
@@ -85,7 +122,8 @@ const Footer = () => {
       </div>
       <div className="bg-customGray flex py-6 justify-center items-center">
         <p className="text-gray-400 text-center lg:p-0 px-1">
-          Upfront is a part of the Catalyst Group. Copyright 2024. All Rights Reserved.
+          Upfront is a part of the Catalyst Group. Copyright{" "}
+          {new Date().getFullYear()}. All Rights Reserved.
         </p>
       </div>
     </>
